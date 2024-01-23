ac79b2278d0b97e3a9fb52412b5cd7bf8d7df318
i2124018 <104130588+i2124018@users.noreply.github.com>
2024-01-24 01:09:46 +0900

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,69 @@
+//スクロール量の検出
+let topHeight = document.getElementById("top").offsetHeight;
+console.log(topHeight);
+
+window.addEventListener('scroll', function(){
+
+    let scroll = window.scrollY;
+    let headerText = document.querySelector(".nav-list");
+    let sheets = document.styleSheets
+    let sheet = sheets[sheets.length - 1];
+
+    //スタイルルールの追加
+
+    if(scroll > topHeight - 50){
+        headerText.style.color = "#333";
+        sheet.insertRule(
+            '.nav-list-item a::before { background-Color: #333 }',
+            sheet.cssRules.length
+        );
+    }else if(scroll < topHeight - 50){
+        headerText.style.color = "#fdfdfd";
+        sheet.insertRule(
+            '.nav-list-item a::before { background-Color: #fdfdfd }',
+            sheet.cssRules.length
+        );
+    }
+});
+
+const pie = document.querySelector(".pie");
+
+function skillsAnimation(entries){
+    let count;
+    if (entries[0].intersectionRatio == 0 && b) {
+
+    }else{
+        let angle = 0;
+
+        function drawCircle(){
+        if(angle <  110){
+                angle += 4;
+                pie.style.backgroundImage = `conic-gradient(black ${angle}deg, white ${angle}deg)`;
+                requestAnimationFrame(drawCircle);
+            }else if(angle <  220){
+                angle += 2;
+                pie.style.backgroundImage = `conic-gradient(black ${angle}deg, white ${angle}deg)`;
+                requestAnimationFrame(drawCircle);
+            }
+            else if(angle <  230){
+                angle += 1;
+                pie.style.backgroundImage = `conic-gradient(black ${angle}deg, white ${angle}deg)`;
+                requestAnimationFrame(drawCircle);
+            }
+        };
+    
+        requestAnimationFrame(drawCircle);
+        b = false
+    }
+}
+
+let options = {
+
+    threshold:1
+    }
+    
+    ;
+    
+let observer = new IntersectionObserver(skillsAnimation,options);
+    
+    observer.observe(pie);
---
+++ assets/js/scrollreveal.js
@@ -0,0 +1,4 @@
+ScrollReveal({reset: false, distance: "60px", duration: 2000})
+ScrollReveal().reveal(".top-img", {delay:200, origin:"top"})
+ScrollReveal().reveal(".top-text", {delay:400, origin:"left", distance: "200px"})
+ScrollReveal().reveal("header", {delay:1000, origin:"top", distance: "100px"})
