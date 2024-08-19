9081d2199322125c37914fb408e61faaec71773c
i2124018 <104130588+i2124018@users.noreply.github.com>
2024-08-19 18:22:04 +0900

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,61 +12,80 @@ window.addEventListener('scroll', function(){
 
     //スタイルルールの追加
 
-    if(scroll > topHeight - 50){
-        headerText.style.color = "#333";
-        sheet.insertRule(
-            '.nav-list-item a::before { background-Color: #333 }',
-            sheet.cssRules.length
-        );
-        arrow.style.visibility = "visible";
-    }else if(scroll < topHeight - 50){
+    // if(scroll > topHeight - 50){
+    //     headerText.style.color = "#333";
+    //     sheet.insertRule(
+    //         '.nav-list-item a::before { background-Color: #333 }',
+    //         sheet.cssRules.length
+    //     );
+    //     arrow.style.visibility = "visible";
+    // }else if(scroll < topHeight - 50){
+    //     headerText.style.color = "#fdfdfd";
+    //     sheet.insertRule(
+    //         '.nav-list-item a::before { background-Color: #fdfdfd }',
+    //         sheet.cssRules.length
+    //     );
+    //     arrow.style.visibility = "hidden";
+    // }
+    if(scroll < topHeight - 2/10*topHeight && scroll > topHeight - 11/12*topHeight){
         headerText.style.color = "#fdfdfd";
+        
         sheet.insertRule(
             '.nav-list-item a::before { background-Color: #fdfdfd }',
             sheet.cssRules.length
         );
-        arrow.style.visibility = "hidden";
+
+    }else{
+        headerText.style.color = "#333";
+        
+        sheet.insertRule(
+            '.nav-list-item a::before { background-Color: #333 }',
+            sheet.cssRules.length
+        );
     }
 });
 
-const pie = document.querySelector(".pie");
 
-function skillsAnimation(entries){
-    let count;
-    if (entries[0].intersectionRatio == 0 && b) {
 
-    }else{
-        let angle = 0;
 
-        function drawCircle(){
-        if(angle <  110){
-                angle += 4;
-                pie.style.backgroundImage = `conic-gradient(black ${angle}deg, white ${angle}deg)`;
-                requestAnimationFrame(drawCircle);
-            }else if(angle <  220){
-                angle += 2;
-                pie.style.backgroundImage = `conic-gradient(black ${angle}deg, white ${angle}deg)`;
-                requestAnimationFrame(drawCircle);
-            }
-            else if(angle <  230){
-                angle += 1;
-                pie.style.backgroundImage = `conic-gradient(black ${angle}deg, white ${angle}deg)`;
-                requestAnimationFrame(drawCircle);
-            }
-        };
+// const pie = document.querySelector(".pie");
+
+// function skillsAnimation(entries){
+//     let count;
+//     if (entries[0].intersectionRatio == 0 && b) {
+
+//     }else{
+//         let angle = 0;
+
+//         function drawCircle(){
+//         if(angle <  110){
+//                 angle += 4;
+//                 pie.style.backgroundImage = `conic-gradient(black ${angle}deg, white ${angle}deg)`;
+//                 requestAnimationFrame(drawCircle);
+//             }else if(angle <  220){
+//                 angle += 2;
+//                 pie.style.backgroundImage = `conic-gradient(black ${angle}deg, white ${angle}deg)`;
+//                 requestAnimationFrame(drawCircle);
+//             }
+//             else if(angle <  230){
+//                 angle += 1;
+//                 pie.style.backgroundImage = `conic-gradient(black ${angle}deg, white ${angle}deg)`;
+//                 requestAnimationFrame(drawCircle);
+//             }
+//         };
     
-        requestAnimationFrame(drawCircle);
-        b = false
-    }
-}
+//         requestAnimationFrame(drawCircle);
+//         b = false
+//     }
+// }
 
-let options = {
+// let options = {
 
-    threshold:1
-    }
+//     threshold:1
+//     }
     
-    ;
+//     ;
     
-let observer = new IntersectionObserver(skillsAnimation,options);
+// let observer = new IntersectionObserver(skillsAnimation,options);
     
-    observer.observe(pie);
+//     observer.observe(pie);
